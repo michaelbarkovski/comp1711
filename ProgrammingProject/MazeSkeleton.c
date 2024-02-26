dde3fcbd298e85cc6813aa91e07d6c26a01b39d9
Michail Barkovskis <146209276+michaelbarkovski@users.noreply.github.com>
2024-02-26 17:29:45 +0000

    coursework

---
+++ ProgrammingProject/MazeSkeleton.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+//defining the acceptible dimensions for the maze as described in the prompt
+#define MaxHeight 100
+#define MinHeight 5
+#define MaxWidth 100
+#define minWidth 5
+
+typedef struct{
+    int Maze_height;
+    int Maze_width;
+
+
+} Maze;
+
+int load_file(){
+    // code to load in the maze file written here
+    // function should check if the file can be opened, and return 1 with an error message if it cannot
+    // program should also check if the dimensions of the Maze are within the given range, defined at the start of the program
+}
+
+void display_Maze() {
+    // code here should display the maze on the terminal
+
+}
+
+void move(){
+    // code here should handle the moves of the player
+}
+
+
+
+int main(){
+    
+}
